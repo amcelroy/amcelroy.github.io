234421923ccee24d4d6cb45755d4d3841226bc01
Austin McElroy <mcelroy.austin@gmail.com>
2024-01-11 21:00:36 -0600

    More pathing

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	assetsInclude: ['**/*.pdf'],
-	base: '/austin.engineer/build/',
+	base: '/austin.engineer/',
 	plugins: [
 		sveltekit(), 
 		purgeCss(),
-	]
+	],
 });
