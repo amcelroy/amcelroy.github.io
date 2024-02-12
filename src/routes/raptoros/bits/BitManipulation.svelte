6a0c6913765589cbe6207513314a78f951041dd3
Austin McElroy <mcelroy.austin@gmail.com>
2024-02-11 21:51:33 -0600

    Fixed bug with mutliple Bitlib windows

--- src/routes/raptoros/bits/BitManipulation.svelte
+++ src/routes/raptoros/bits/BitManipulation.svelte
@@ -8,15 +8,11 @@
     
     let finalized = false;
 
-    let value: number = 0;
     let value_0_7: number = 0;
     let value_8_15: number = 0;
     let value_16_23: number = 0;
     let value_24_31: number = 0;
-
-    // $: value = BigInt(value_0_7);
-    // $: bits_16 = BigInt((value_8_15 << BigInt(8)) + value_0_7);
-    // $: bits_32 = BigInt(((BigInt(value_24_31) << BigInt(24)) + (BigInt(value_16_23) << BigInt(16)) + (BigInt(value_8_15) << BigInt(8)) + BigInt(value_0_7)));
+    let inputString: string = "";
 
     let bitArray: boolean[] = [];
 
@@ -33,69 +29,78 @@
         let _16_bit = (BigInt(value_8_15) << BigInt(8)) + BigInt(value_0_7);
         let _32_bit = (BigInt(value_24_31) << BigInt(24)) + (BigInt(value_16_23) << BigInt(16)) + (BigInt(value_8_15) << BigInt(8)) + BigInt(value_0_7);
 
-        let ele8 = document.getElementById("bits8input"); 
-        if(ele8) {
-            ele8.value = value_0_7.toString();
+        switch(bits) {
+            case 8:
+                setInputValue(_8_bit);
+                break;
+            case 16:
+                setInputValue(_16_bit);
+                break;
+            case 32:
+                setInputValue(_32_bit);
+                break;
         }
-
-        let ele16 = document.getElementById("bits16input");
-        if(ele16) {
-            ele16.value = _16_bit.toString();
-        }
-
-        let ele32 = document.getElementById("bits32input");
-        if(ele32) {
-            ele32.value = _32_bit.toString();
-        }
-
         console.log(changes);
     }
 
     function onLeftShift()  {
-        // value = value << BigInt(1);
-        // bits_16 = bits_16 << BigInt(1);
-        // bits_32 = bits_32 << BigInt(1);
-        // let max = BigInt(2) ** BigInt(bits);
-        // if(value >= max) {
-        //     value = max;
-        //     bits_16 = max;
-        //     bits_32 = max;
-        // }
-        console.log("TODO: Left Shift");
+        let v = getInputValue();
+        v = v << BigInt(1);
+        let max = BigInt(2) ** BigInt(bits);
+        if(v >= max) {
+            v = max;
+        }
+        update(v);
+        setInputValue(v);
     }
 
     function onRightShift()  {
-        // value = value >> BigInt(1);
-        // if(value < 0) {
-        //     value = 0;
-        // }
-        console.log("TODO: Right shift")
+        let v = getInputValue();
+        v = v >> BigInt(1);
+        if(v <= 0) {
+            v = 0;
+        }
+        update(v);
+        setInputValue(v);
     }
 
-    function onInputChanged(inputEvent: any) {
-        console.log(inputEvent);
-        let newValue = inputEvent.srcElement.value;
+    function onZeros() {
+        update(0);
+        setInputValue(0);
+    }
+
+    function onOnes() {
+        let max = BigInt(2) ** BigInt(bits) - BigInt(1);
+        update(max);
+        setInputValue(max);
+    }
+
+    function getInputValue() {
+        return BigInt(inputString);
+    }
+
+    function setInputValue(newValue: bigint) {
+        inputString = newValue.toString();
+    }
+
+    function update(newValue: bigint) {
         switch(bits) {
             case 8:
                 if(newValue > 255) {
                     newValue = 255;
                 }
-
                 if(newValue < 0) {
                     newValue = 0;
                 }
-
                 value_0_7 = Number(BigInt(newValue));
                 break;
             case 16:
                 if(newValue > 65535) {
                     newValue = 65535;
                 }
-
                 if(newValue < 0) {
                     newValue = 0;
                 }
-
                 value_0_7 = Number(BigInt(newValue) & BigInt(0xFF));
                 value_8_15 = Number((BigInt(newValue) & BigInt(0xFF00)) >> BigInt(8));
                 break;
@@ -103,17 +108,20 @@
                 if(newValue > 4294967295) {
                     newValue = 4294967295;
                 }
-
                 if(newValue < 0) {
                     newValue = 0;
                 }
-
                 value_0_7 = Number(BigInt(newValue) & BigInt(0xFF));
                 value_8_15 = Number(BigInt(newValue) & BigInt(0xFF00) >> BigInt(8));
                 value_16_23 = Number(BigInt(newValue) & BigInt(0xFF0000) >> BigInt(16));
                 value_24_31 = Number(BigInt(newValue) & BigInt(0xFF000000) >> BigInt(24));
                 break;
         }   
+    }
+
+    function onInputChanged(inputEvent: any) {
+        let newValue = BigInt(inputEvent.srcElement.value);
+        update(newValue);
     }
     
 </script>
@@ -139,18 +147,14 @@
     </div>
 {:else}
 
-    {#if bits == 8}
-        <input id="bits8input" class="tui-input number" type="text" inputmode="numeric" pattern="\d*" value="{value.toString()}" on:change={onInputChanged}/>
-    {:else if bits == 16}
-        <input id="bits16input" class="tui-input number" type="text" inputmode="numeric" pattern="\d*" value="{value.toString()}" on:change={onInputChanged}/>
-    {:else if bits == 32}
-        <input id="bits32input" class="tui-input number" type="text" inputmode="numeric" pattern="\d*" value="{value.toString()}" on:change={onInputChanged}/>
-    {/if}
+    <input class="tui-input number" type="text" inputmode="numeric" pattern="\d*" value={inputString} on:change={onInputChanged}/>
 
     <br/>
     <br/>
-    <!-- <button class="tui-button" on:click={onLeftShift}>&lt&lt;</button>
-    <button class="tui-button" on:click={onRightShift}>&gt&gt;</button> -->
+    <button class="tui-button button_margin" on:click={onLeftShift}>&lt&lt;</button>
+    <button class="tui-button button_margin" on:click={onRightShift}>&gt&gt;</button>
+    <button class="tui-button button_margin" on:click={onZeros}>0's</button>
+    <button class="tui-button button_margin" on:click={onOnes}>1's</button>
     <br/>
     <br/>
 
@@ -165,8 +169,6 @@
         <BitGrouping startBit={8} on:change={onBitGroupingChanged} bind:input={value_8_15}/>
         <BitGrouping startBit={0} on:change={onBitGroupingChanged} bind:input={value_0_7}/>
     {/if}
-
-
 {/if}
 
 
