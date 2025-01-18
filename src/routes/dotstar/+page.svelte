2925c72b4435a7fde81b6c4847f92b9a9199c881
Austin McElroy <mcelroy.austin@gmail.com>
2025-01-18 15:51:35 -0600

    Updated some text

--- src/routes/dotstar/+page.svelte
+++ src/routes/dotstar/+page.svelte
@@ -146,26 +146,24 @@
     </div>
     <div class="shadow border m-8 rounded-lg variant-glass-surface p-2">
         <p class="m-3">
-            I wanted a place to show off my watercolor paintings and decided to build a picture frame use the Dotstar LEDs to illuminate the light. I'm trying
-            to make a shadowbox but with some more dynamic lighting to make the optics part of the art. Since there wasn't anything that seemed to work for my on 
-            Amazon, it was time to put my EE degree to work. I made a custom PCB that uses the Fanstel nRF52840 BLE MCU 
+            I wanted a place to show off my watercolor paintings and decided to build a picture frame using Adafruit Dotstar LEDs to illuminate the art. Something likely
+            a shadowbox but with some more dynamic lighting to make the optics part of the art. Since there wasn't anything that seemed to work for me on 
+            Amazon, it was time to put that EE degree to work. I made a custom PCB that uses the Fanstel nRF52840 BLE MCU 
             module, a high capacity capacitor, 3.3V regulator, and USB-C connector for power - up to 2.9A at 5V. The Dotstar LEDs require 5V for power and logic, 
-            so I added a level shifter as well to covert the 3.3V from the nRF to 5V. 
+            so I added a Texas Instruments TXS0102 level shifter as well to covert the 3.3V logic from the nRF to 5V. 
         </p>
         <p class="m-3">
-            The nRF52840 is using Rust (of course) Embassy firmware and BLE stack that controls the waveforms that is displayed on the Dotstar LEDs. I decided to
-            have each LED controlled by a waveform that can be dynamically updated - the delta time (dt) of the update along with the amplitude, frequency, phase, 
+            The nRF52840 is using Rust (of course) Embassy firmware and BLE stack that controls the waveforms that are displayed on the Dotstar LEDs. I decided to
+            have each LED controlled by a waveform that can be dynamically updated - the amplitude, frequency, phase, 
             and offset. The waveforms should generally reside between -1 to 1, which maps from 0 -%gt 255 in 
             the 8-bit space. The superposition of the waveforms maps to color. We have smooth dynamic, programmable, lighting. Let there be light!
         </p>
         <p class="m-3">
             One of the cool things about embedded Rust is working in with core and no_std libraries. This usually means a small feature set with minimal
             memory allocation on the heap. There are heap allocators in Rust for no_std, but generally the more that is known at compile time the 
-            better things will be. This also means fewer concurrency options to safely share data.
-        </p>
-        <p class="m-3">
-            Web Assembly, WASM, has similar limitations to the embedded space. This means libraries written in no_std Rust can be exported relatively 
-            easily to WASM. This project is a library that I have been working on to control the Adafruit Dotstar LED strips. They have an addressable
+            better things will be. This also means fewer concurrency options to safely share data. Web Assembly, WASM, has similar limitations to the embedded space. 
+            This means libraries written in no_std Rust can be exported relatively 
+            easily to WASM. The waveforms above are generated in a Rust library that I have been working on to control the Adafruit Dotstar LED strips. They have an addressable
             LED protocol that has a start frame, a series of digital data for each LED, and an end frame. Each white light or RGB LED takes 32 bits of data
             to program. the 3 upper bytes are 1, followed by 5 bits for a global alpha, followed by 8 bits for blue, green, and red (or white, white, white
             for the mono-color). The update rate is really high using the SPI bus with a core clock speed of 8 MHz means we can spit out data at
@@ -173,7 +171,7 @@
             at 50ms (20 Hz) govern the update rate of the LEDs.
         </p>
         <p class="m-3">
-            Basically, this code is the same that is running on the MCU and controlling the physical LEDs. Enjoy messing around with the waveforms and making 
+            tl,dr; this code is the same that is running on the MCU and controlling the physical LEDs. Enjoy messing around with the waveforms and making 
             a pattern that could work on a physical light strip!
         </p>
     </div>
