e8fb83cb4ab5ac8299b8645ef145fb47a620d5f5
Austin McElroy <mcelroy.austin@gmail.com>
2025-02-18 22:10:45 -0600

    New font, intro page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,32 +3,26 @@
 </script>
 
 <div class="card intro variant-glass-primary">
-    <p>
-        This website is a work in progress. This is a SvelteKit SSR project compiled as a static site and hosted 
-        on Github Pages. It is a place to showcase my work and hobbies and keep some engineering tools handy. Check out 
-        my resume, publications, patents, StackOverflow profile, Github profile, and projects; shoot me an email if you 
-        have any questions.
-    </p>
 
     <br/>
 
     <p>
-        I have a B.S. in Electrical Engineer from the University of Texas, Austin, and finished up a M.S.E.E at UT during
-        the COVID pandemic. My masters thesis was part of a team; I designed the circuit board and firmware for a low-cost bridge 
-        ventilator unit. The team was lead by Dr. Thomas Milner and Dr. Johnathan Valvano and couldn't have been completed without
-        the help of a lot of other people. 
-        Though we moved fast, the vaccine came out as we were submitting the device to the FDA under the emergancy authorization
-        act and it was deemed not needed. Regardless, it was an amazing experience that made us feel empowered during a time of a 
-        lot of uncertainty.
+        Howdy. My name is Austin and I live in Austin. 
+    </p>
+    <p>
+        I have a B.S. in Electrical Engineer (2004) from the University of Texas, Austin, and a M.S.E.E. from UT
+        Austin (2021). I've had a pretty awesome career working with a lot of great people and getting to learn
+        about a lot of different topics: optics, high speed computing, analog circuit design, controls, machine learning,
+        and since finishing my M.S.E.E., I've been working on embedded systems, firmare, Rust, large language models, 
+        mesh nebulizer drive circuits, digital PCB design, including my first PCBs with MCUs and BLE.
     </p>
 
-    <br/>
-
-    <p style="color: lightgreen;">
-        <a href="https://news.utexas.edu/2020/04/01/solving-the-ventilator-shortage-with-windshield-wiper-parts/">ABBU Press Release</a>
+    <p>
+        Programming has become a passion, hobby, and career, doing projects in: C, Labview, C++, CUDA, OpenCL, Python, Typescript,
+        and Rust. Mostly in that order. Most of my work now is Rust, Typescript, and Python. All of my firmware in the 
+        last few years is exclusively in Rust, including hobby projects like the picture frame and supporting libraries
+        to run the <code>dotstar</code> LEDs
     </p>
-
-    <br/>
 
     <p>
         I'm a Senior Software Engineer at Pictorus working on bringing Rust to model based design system for embedded systems. My current task is trying
@@ -36,7 +30,23 @@
         the Rust project. It has been an amazing learning experience for CI/CD, Rust, generated code, and more. 
     </p>
 
-    <br/>
+    <p>
+        Most of my previous work has been in biomedical R and D, working on a variety of projects from Optical Coherence Tomography (OCT)
+        imaging systems, speckle flood flow imaging, pace maker detectors, multi-mesh nebulizers, cardiac admittance volume
+        measurement systems, 3D printer laser sintering control systems and high speed imaging, and more.
+    </p>
+
+    <p>
+        Hobbies: mountain biking (south Austin has a LOT of trails), roller blading, any biking with my son, date night with my wife, visiting with family,
+        3D printing, PCB design, firmware, any facet of electrical engineering, learning, trying to be a good human.
+    </p>
+
+    <p>
+        This website is a work in progress. It is a SvelteKit SSR project compiled as a static site and hosted 
+        on Github Pages. It is a place to showcase my work and hobbies and keep some engineering tools handy. Check out 
+        my resume, publications, patents, StackOverflow profile, Github profile, and projects; shoot me an email if you 
+        have any questions.
+    </p>
 
     <p>
         I wrote this website using SvelteKit using the <a href="https://www.skeleton.dev/" style="color: lightgreen;">Skeleton UI library</a>. They
@@ -44,17 +54,31 @@
         and the retro DOS feel seemed perfect. Thanks to all the folks at Skeleton UI and TuiCss and all the other dependencies I used.
     </p>
 
-    <br/>
-
     <p>
         "If I have seen further it is by standing on the shoulders of Giants." - Isaac Newton
     </p>
 </div>
 
-<style>
+<style global>
     .intro {
         margin: 16px;
         padding: 16px;
+    }
+
+    p {
+        padding: 16px;
+    }
+
+    @font-face {
+        font-family: 'Fira Sans';
+        src: url('$lib/FiraCode-VariableFont_wght.ttf') format('ttf');
+    }
+
+    code {
+        color:orange;
+        font-family: 'Fira Sans', sans-serif;
+        font-weight: 400;
+        font-size: 16px;
     }
 
 </style>
